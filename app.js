3eac393f35a349741de1e3cce362540735096895
Trey Rader <treyrdr09@gmail.com>
2017-03-11 12:29:12 -0600

    added event handler for form template

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+Items = new Mongo.Collection('items');
+
 if (Meteor.isClient) {
 	Template.body.helpers({
-		items: [
-			{item: "Oranges"}
-		]
+		getItems: function() {
+			return Items.find();
+		}
 	});
+
+	Template.addItem.events({
+		'submit .new-item': function(event) {
+			var item = event.target.title.value;
+			console.log(item);
+			Items.insert({
+				item: item,
+				createdAt: new Date()
+			});
+
+			return false;
+		}
+	})
 }
 
 if(Meteor.isServer) {
-	
+
 }
