813b16d2ad8a34f49c555b52dc538371d7d0551a
Trey Rader <treyrdr09@gmail.com>
2017-03-12 17:24:57 -0500

    added login form/added bootstrap fixed nav

--- app.js
+++ app.js
@@ -10,21 +10,42 @@ if (Meteor.isClient) {
 
 	Template.addItem.events({
 		'submit .new-item': function(event) {
-			var item = event.target.title.value;
+			//var item = event.target.text.value;
+			var item = $('.new-item').val();
 			console.log(item);
 			Items.insert({
-				item: "dummy-data",
+				item: item,
 				createdAt: new Date()
 			});
 
-			event.target.title.value = "";
+			//event.target.text.value = "";
 			return false;
 		},
-		'click #arrow-down': function(event) {
+		'click #arrow-down': function() {
 			$('#needed-list').slideToggle('slow');
 		}
 	})
+
+	Template.item.events({
+		'click .right-icon': function() {
+			Items.remove(this._id);
+		},
+		'click .left-icon': function() {
+			Items.update(this._id, {$set:{checked: !this.checked}});
+		},
+		'mouseenter .item': function(event) {
+			$('.right-icon').show();
+			$('.left-icon').show();
+			//console.log(Items.findOne(this._id));
+		},
+		'mouseleave .item': function() {
+			$('.right-icon').hide();
+			$('.left-icon').hide();
+		}
+	})
+
 }
+
 
 if(Meteor.isServer) {
 
