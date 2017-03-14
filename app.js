b84b96cdab7a07e585ed9a66da55ab72caeaab45
Trey Rader <treyrdr09@gmail.com>
2017-03-13 19:27:28 -0500

    navbar styles changed with new functionality; cover image taken out and increased fontsize

--- app.js
+++ app.js
@@ -1,21 +1,67 @@
+
 Items = new Mongo.Collection('items');
 
 if (Meteor.isClient) {
-	Template.body.helpers({
-		items: function() {
-			return Items.find();
-			//console.log(Items.find());
+
+	Meteor.subscribe('items');
+
+	Template.navbar.helpers({
+		hideChecked: function() {
+			return Session.get('hideChecked');
 		}
+	})
+
+	Template.shoppingList.helpers({
+		items: function() {
+			if(Session.get('hideChecked')) {
+				return Items.find({checked: {$ne: true}});
+			}
+			 else {
+			 	return Items.find();
+			 }
+		},
+		hideChecked: function() {
+			return Session.get("hideChecked");
+			//need helper to return session variable value to template so that you can put in if statement. 
+			//any data that isn't already avaiable or visible in the template as is, you'll need to return.
+		},
+		isOwner: function() {
+			return this.owner === Meteor.userId();
+		}
+	});
+
+	Template.navbar.events({
+		'click span[data-action="hide-checked"]': function() {
+			Session.set("hideChecked",true);
+		},
+		'click span[data-action="show-checked"]': function(){
+			Session.set("hideChecked",false);
+		}
+	})
+
+	Template.shoppingList.events({
+		'click .right-icon': function() {
+			Meteor.call('deleteItem', this._id);
+		},
+		'click .left-icon': function() {
+			Meteor.call('updateItem', this._id, !this.checked);
+			//Items.update(this._id, {$set:{checked: !this.checked}});
+		},
+		'mouseenter .item': function(event) {
+			//console.log(event)
+			$(event.target).children('.right-icon').show();
+			$(event.target).children('.left-icon').show();
+		},
+		'mouseleave .item': function() {
+			$('.right-icon').hide();
+			$('.left-icon').hide();
+		} 
 	});
 
 	Template.addItem.events({
 		'submit .new-item': function(event) {
 			var item = event.target.item.value;
-			console.log(item);
-			Items.insert({
-				item: item,
-				createdAt: new Date()
-			});
+			Meteor.call('addItem', item);
 
 			event.target.item.value = "";
 			return false;
@@ -23,25 +69,7 @@ if (Meteor.isClient) {
 		'click #arrow-down': function() {
 			$('#needed-list').slideToggle('slow');
 		}
-	})
-
-	Template.item.events({
-		'click .right-icon': function() {
-			Items.remove(this._id);
-		},
-		'click .left-icon': function() {
-			Items.update(this._id, {$set:{checked: !this.checked}});
-		},
-		'mouseenter .item': function(event) {
-			$('.right-icon').show();
-			$('.left-icon').show();
-			//console.log(Items.findOne(this._id));
-		},
-		'mouseleave .item': function() {
-			$('.right-icon').hide();
-			$('.left-icon').hide();
-		}
-	})
+	});
 
 	Accounts.ui.config({
 		passwordSignupFields: "USERNAME_ONLY"
@@ -51,5 +79,27 @@ if (Meteor.isClient) {
 
 
 if(Meteor.isServer) {
-
+	Meteor.publish('items', function() {
+		return Items.find();
+	})
 }
+
+Meteor.methods({
+	addItem: function(item) {
+		Items.insert({
+			item: item,
+			createdAt: new Date(),
+			owner: Meteor.userId()
+		});
+	},
+	updateItem: function(id, checked) {
+		Items.update(id, {$set: {checked: checked}})
+	},
+	deleteItem: function(id) {
+		Items.remove(id);
+	}
+})
+
+
+
+
